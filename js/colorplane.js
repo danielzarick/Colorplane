b6b9bf765b44cf9372a0fb5306feae566ab91b7f
danielzarick <daniel.zarick@gmail.com>
2013-03-20 15:58:21 -0500

    Finalized colorplane cleanup

--- js/colorplane.js
+++ js/colorplane.js
@@ -12,31 +12,36 @@ $(function(){
     // Set the default color for #colorplane-selected-color in your CSS or
     // with a variable in your view template. Use this to match your brand
     // or to fill it in with the already stored color.
-    var bkgndColor = $('#colorplane-selected-color').css("background-color");
+    var bkgndColor = $('.colorplane-selected-color').css("background-color");
     ctx.fillStyle = bkgndColor;
     ctx.fillRect(0,0,480,220);
 
-    // Simplistic implementation to make 'touchmove' work on smartphones/tablets
+    // Simplistic implementation to get the 'touchmove' event on a touchscreen
     $('#colorplane-canvas').bind('touchmove', function(e) {
         e.preventDefault();
         var touch = e.originalEvent.changedTouches[0];
-        currentColor(touch);
+        var hexColor = getHexColor(touch);
+        showHexColor(hexColor);
 
+        // This will save the current color when the 'touchmove' event ends,
+        // which happens when the finger is lifted or leaves the edge of the screen.
         $('#colorplane-canvas').bind('touchend', function() {
-            selectColor(hexColor);
+            saveCurrentColor(hexColor);
         });
     });
 
+    // This gets the mouse location (hopefully that is obvious)
     $('#colorplane-canvas').mousemove(function(e) {
-        currentColor(e);
+        var hexColor = getHexColor(e);
+        showHexColor(hexColor);
 
-        $('#colorplane-canvas').click(function(hexColor) {
-            selectColor(hexColor);
+        $('#colorplane-canvas').click(function() {
+            saveCurrentColor(hexColor);
         });
     });
 
     // This accepts the mouse or touch location and shows the current color based on X,Y coordinates
-    function currentColor(e) {
+    function getHexColor(e) {
         var canvasOffset = $(canvas).offset();
         var X = e.pageX - canvasOffset.left;
         var Y = e.pageY - canvasOffset.top;
@@ -50,23 +55,24 @@ $(function(){
         var blueHex = normalizeHex(canvasZ);
 
         var hexColor = "#" + redHex + greenHex + blueHex;
+        return hexColor;
+    }
 
+    function showHexColor(hexColor) {
         ctx.fillStyle = hexColor;
         ctx.fillRect(0,0,480,220);
         $('#colorplane-current-hex').html(hexColor);
         $('#colorplane-current-hex').css("color", hexColor);
-
-        return hexColor;
     }
 
     // This is triggered on a mouse click or touchend. Change the IDs below
     // to match the form field where you want the user to input a HEX color
     // For best results, hide the form field from the user so all they see
     // is the color that they have selected.
-    function selectColor(hexColor) {
+    function saveCurrentColor(hexColor) {
         $('#colorplane-selected-hex').html(hexColor);
         $('#colorplane-selected-hex').css("color", hexColor);
-        $('#colorplane-selected-color').css("background", hexColor);
+        $('.colorplane-selected-color').css("background", hexColor);
         $('.picker-instructions').html("Color selected!");
     }
 
